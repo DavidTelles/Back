7e5ba4955cfa3c1e51d640e33783724dcaa6db2a
David Telles <you@example.com>
2026-04-23 15:10:27 -0300

    bcrypt

--- apiLogin/routes/users.js
+++ apiLogin/routes/users.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const userRouters = express.Router();
 const db = require('../db');
+const bcrypt = require('bcrypt');
 
-userRouters.get('/', (req, res) => {
+userRouters.get('/get', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) {
             res.status(500).json({ error: 'Erro ao buscar usuários '});
@@ -11,5 +12,18 @@ userRouters.get('/', (req, res) => {
         };
     });
 });
+
+userRouters.post('/create', async (req, res) => {
+    const { name, email, password } = req.body;
+    const senhaHash = await bcrypt.hashSync(senha, 10)
+    db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password], (wee, results) => {
+        if (err) {
+            console.error("ERROR: ", err);
+            res.status(500).json({ error: 'Erro ao criar usuários'});
+        } else {
+            res.json(results);
+        }
+    });
+})
 
 module.exports = userRouters;
